9baaf44a851fdeb408b377b88ebecc46404a469e
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-02-11 22:25:57 +0530

    refactor: moved montre config file to examples dir

--- main.go
+++ main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-
 	"github.com/ghoshRitesh12/montre/lib"
 )
 
 func main() {
-	montre := lib.Init()
-	montre.StartWatching()
-
-	os.Exit(0)
-
-	fmt.Println("(go pid) or ppid", os.Getpid())
-
-	cmd := exec.Command("node", "index.js")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Start(); err != nil {
-		fmt.Println("some error occured:", err.Error())
-		return
-	}
-
-	fmt.Println("node pid", cmd.Process.Pid)
-
-	if err := cmd.Wait(); err != nil {
-		fmt.Println("process finished with error:", err.Error())
-		return
-	}
-
-	fmt.Println(cmd.Path)
+	lib.Init().StartWatching()
 }
